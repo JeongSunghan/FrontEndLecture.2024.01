ef5f1ba6efb0d79eac4746c391f41903da71244e
JeongSunghan <jsh000311@gmail.com>
2024-01-04 17:30:23 +0900

    Commit message

--- JavaScript/ch03.조건문/99.alarm.js
+++ JavaScript/ch03.조건문/99.alarm.js
@@ -39,3 +39,20 @@ function timer(h, m) {
   console.log(timer(0, 30))
   console.log(timer(23, 40))
 
+//teacher answer
+let hour = 6, minute = 30;
+let newHour = 0, newMin = 0;
+
+if (minute >=45) {
+  newHour = hour;
+  newMin = minute - 45 ;
+} else {
+  if (hour == 0) {
+    newHour = 23;
+    newMin = minute + 60 - 45 ;
+  } else{
+    newHour = hour - 1;
+    newMin = minute + 60 - 45 ;
+  }
+}
+console.log(`${hour}시 ${minute}분 --> ${newHour}시 ${newMin}분`);
