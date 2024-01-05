ef717586b4a335beb531c225398bbcd48efa2194
JeongSunghan <jsh000311@gmail.com>
2024-01-05 12:43:22 +0900

    Commit message

---
+++ JavaScript/ch05.함수/Q1.문제.js
@@ -0,0 +1,16 @@
+// q1. 배열에 대한 함수
+// 1. 배열을 인수로 받아서, 합을 반환하는 함수
+function sumArray(arr) {
+    
+    
+}
+
+// 2. 배열에서 최댓값을 찾기.
+function maxArray(arr) {
+    
+
+}
+
+let sampleArray = [43, 17, 25, 36, 98, 93];
+console.log(sumArray(sampleArray));
+console.log(maxArray(sampleArray));
